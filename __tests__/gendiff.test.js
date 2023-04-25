05cdc399072ea5b4da6963eeb7f6794f161ff3c3
liya <liyarheon@gmail.com>
2023-04-25 20:19:14 +0300

    test

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,21 +1,23 @@
-import gendiff from '../src/index';
+import gendiff from "../src/index";
 
-test('проверка на нахождение разности в файлах с разными расширениями', () => {
-  const expected = '-follow: false\nhost: hexlet.io\n-proxy: 123.234.53.22\n+pages: 224\n-timeout: 50\n+timeout: 20\n+verbose: true';
+test("проверка на нахождение разности в файлах с разными расширениями", () => {
+  const expected =
+    "-follow: false\nhost: hexlet.io\n-proxy: 123.234.53.22\n+pages: 224\n-timeout: 50\n+timeout: 20\n+verbose: true";
   expect(
     gendiff(
-      '__tests__/test.file.json',
-      '/home/liya/frontend-project-46/__tests__/test.file.yml',
-    ),
+      "__tests__/test.file.json",
+      "/home/liya/frontend-project-46/__tests__/test.file.yml"
+    )
   ).toEqual(expect.stringContaining(expected));
 });
 
-test('проверка на нахождение разности в файлах с разными путями', () => {
-  const expected = '-follow: false\nhost: hexlet.io\n-proxy: 123.234.53.22\n-timeout: 50\n+timeout: 20\n+verbose: true';
+test("проверка на нахождение разности в файлах с разными путями", () => {
+  const expected =
+    "-follow: false\nhost: hexlet.io\n-proxy: 123.234.53.22\n-timeout: 50\n+timeout: 20\n+verbose: true";
   expect(
     gendiff(
-      '__tests__/test.file.json',
-      '/home/liya/frontend-project-46/__fixtures__/file2.json',
-    ),
+      "__tests__/test.file.json",
+      "/home/liya/frontend-project-46/__fixtures__/file2.json"
+    )
   ).toEqual(expect.stringContaining(expected));
 });
