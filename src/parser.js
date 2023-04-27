00bf8b648b4863ef969d62e8bc151e49e9b9a845
liya <liyarheon@gmail.com>
2023-04-27 21:33:53 +0300

    test3

--- src/parser.js
+++ src/parser.js
@@ -1,14 +1,14 @@
 /* eslint-disable consistent-return */
 /* eslint-disable import/no-cycle */
 /* eslint-disable default-case */
-import yaml from 'js-yaml';
-import { readFile, extension } from './index';
+import yaml from "js-yaml";
+import { readFile, extension } from "./index.js";
 
 const parser = (file) => {
   switch (extension(file)) {
-    case '.json':
+    case ".json":
       return JSON.parse(readFile(file));
-    case '.yml':
+    case ".yml":
       return yaml.load(readFile(file));
   }
 };
