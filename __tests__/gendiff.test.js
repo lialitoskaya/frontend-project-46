6f66507ae0bf85ad22e6223a6a44f87e4902b8b3
liya <liyarheon@gmail.com>
2023-05-17 20:00:41 +0300

    .

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,6 +1,6 @@
 import { fileURLToPath } from 'url';
 import { dirname, join } from 'path';
-import gendiff from '../src/index.js';
+import gendiff from '../src/stylish.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
