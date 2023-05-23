b53a2d339ffc38aede2b23ff25dd7d749bc4265d
liya <liyarheon@gmail.com>
2023-05-23 22:28:25 +0300

    .

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,8 +1,8 @@
-import _ from "lodash";
-import { keyDiffData } from "../../buildDiff.js";
+import _ from 'lodash';
+import { keyDiffData } from '../../buildDiff.js';
 
 const makeIndent = (node) => {
-  const newNode = node.split("\n");
+  const newNode = node.split('\n');
   return newNode
     .map((str) => {
       if (str !== newNode[0]) {
@@ -10,14 +10,14 @@ const makeIndent = (node) => {
       }
       return str;
     })
-    .join("\n");
+    .join('\n');
 };
 const convert = (tree) => {
   if (_.isObject(tree)) {
     const newString1 = JSON.stringify(tree, null, 4);
     const resultString = makeIndent(newString1)
-      .replaceAll(",", "")
-      .replaceAll('"', "")
+      .replaceAll(',', '')
+      .replaceAll('"', '')
       .trim();
     return resultString;
   }
@@ -26,29 +26,28 @@ const convert = (tree) => {
 
 const stylish = (obj1, obj2) => {
   const newData = keyDiffData(obj1, obj2);
-  const resultdiff = (data) =>
-    data
-      .map((child) => {
-        switch (child.status) {
-          case "unchanged":
-            return `    ${child.key}: ${convert(child.value)}`;
-          case "changed":
-            return `  - ${child.key}: ${convert(child.oldValue)}\n  + ${
-              child.key
-            }: ${convert(child.newValue)}`;
-          case "deleted":
-            return `  - ${child.key}: ${convert(child.value)}`;
-          case "added":
-            return `  + ${child.key}: ${convert(child.value)}`;
-          case "nested":
-            return `    ${child.key}: {\n    ${makeIndent(
-              resultdiff(child.children)
-            )}\n    }`;
-          default:
-            throw new Error();
-        }
-      })
-      .join("\n");
+  const resultdiff = (data) => data
+    .map((child) => {
+      switch (child.status) {
+        case 'unchanged':
+          return `    ${child.key}: ${convert(child.value)}`;
+        case 'changed':
+          return `  - ${child.key}: ${convert(child.oldValue)}\n  + ${
+            child.key
+          }: ${convert(child.newValue)}`;
+        case 'deleted':
+          return `  - ${child.key}: ${convert(child.value)}`;
+        case 'added':
+          return `  + ${child.key}: ${convert(child.value)}`;
+        case 'nested':
+          return `    ${child.key}: {\n    ${makeIndent(
+            resultdiff(child.children),
+          )}\n    }`;
+        default:
+          throw new Error();
+      }
+    })
+    .join('\n');
 
   return `{\n${resultdiff(newData)}\n}`;
 };
