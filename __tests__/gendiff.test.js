36729ce647984650c86d1c8d8216e89e698226a9
liya <liyarheon@gmail.com>
2023-05-24 17:33:09 +0300

    .

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -19,80 +19,80 @@ Property 'group1.nest' was updated. From [complex value] to 'str'
 Property 'group2' was removed
 Property 'group3' was added with value: [complex value]`;
 
-// const expected2 = `{
-//     common: {
-//       + follow: false
-//         setting1: Value 1
-//       - setting2: 200
-//       - setting3: true
-//       + setting3: null
-//       + setting4: blah blah
-//       + setting5: {
-//             key5: value5
-//         }
-//         setting6: {
-//             doge: {
-//               - wow:
-//               + wow: so much
-//             }
-//             key: value
-//           + ops: vops
-//         }
-//     }
-//     group1: {
-//       - baz: bas
-//       + baz: bars
-//         foo: bar
-//       - nest: {
-//             key: value
-//         }
-//       + nest: str
-//     }
-//   - group2: {
-//         abc: 12345
-//         deep: {
-//             id: 45
-//         }
-//     }
-//   + group3: {
-//         deep: {
-//             id: {
-//                 number: 45
-//             }
-//         }
-//         fee: 100500
-//     }
-// }`;
+const expected2 = `{
+    common: {
+      + follow: false
+        setting1: Value 1
+      - setting2: 200
+      - setting3: true
+      + setting3: null
+      + setting4: blah blah
+      + setting5: {
+            key5: value5
+        }
+        setting6: {
+            doge: {
+              - wow:
+              + wow: so much
+            }
+            key: value
+          + ops: vops
+        }
+    }
+    group1: {
+      - baz: bas
+      + baz: bars
+        foo: bar
+      - nest: {
+            key: value
+        }
+      + nest: str
+    }
+  - group2: {
+        abc: 12345
+        deep: {
+            id: 45
+        }
+    }
+  + group3: {
+        deep: {
+            id: {
+                number: 45
+            }
+        }
+        fee: 100500
+    }
+}`;
 
 const expected3 = '[{"key":"common","children":[{"key":"follow","value":false,"status":"added"},{"key":"setting1","value":"Value 1","status":"unchanged"},{"key":"setting2","value":200,"status":"deleted"},{"key":"setting3","oldValue":true,"newValue":null,"status":"changed"},{"key":"setting4","value":"blah blah","status":"added"},{"key":"setting5","value":{"key5":"value5"},"status":"added"},{"key":"setting6","children":[{"key":"doge","children":[{"key":"wow","oldValue":"","newValue":"so much","status":"changed"}],"status":"nested"},{"key":"key","value":"value","status":"unchanged"},{"key":"ops","value":"vops","status":"added"}],"status":"nested"}],"status":"nested"},{"key":"group1","children":[{"key":"baz","oldValue":"bas","newValue":"bars","status":"changed"},{"key":"foo","value":"bar","status":"unchanged"},{"key":"nest","oldValue":{"key":"value"},"newValue":"str","status":"changed"}],"status":"nested"},{"key":"group2","value":{"abc":12345,"deep":{"id":45}},"status":"deleted"},{"key":"group3","value":{"deep":{"id":{"number":45}},"fee":100500},"status":"added"}]';
 
-// test('checking stylish formatter with files of different extensions', () => {
-//   expect(
-//     gendiff(
-//       absolutePath('__fixtures__/newfile3.yaml'),
-//       absolutePath('__fixtures__/newfile1.json'),
-//       'stylish',
-//     ),
-//   ).toEqual(expected2);
-// });
-// test("checking stylish formatter with files with '.json' extensions", () => {
-//   expect(
-//     gendiff(
-//       absolutePath('__fixtures__/newfile.json'),
-//       absolutePath('__fixtures__/newfile1.json'),
-//       'stylish',
-//     ),
-//   ).toEqual(expected2);
-// });
-// test("checking stylish formatter with files with '.yaml' extensions", () => {
-//   expect(
-//     gendiff(
-//       absolutePath('__fixtures__/newfile3.yaml'),
-//       absolutePath('__fixtures__/newfile4.yml'),
-//       'stylish',
-//     ),
-//   ).toEqual(expected2);
-// });
+test.skip('checking stylish formatter with files of different extensions', () => {
+  expect(
+    gendiff(
+      absolutePath('__fixtures__/newfile3.yaml'),
+      absolutePath('__fixtures__/newfile1.json'),
+      'stylish',
+    ),
+  ).toEqual(expected2);
+});
+test.skip("checking stylish formatter with files with '.json' extensions", () => {
+  expect(
+    gendiff(
+      absolutePath('__fixtures__/newfile.json'),
+      absolutePath('__fixtures__/newfile1.json'),
+      'stylish',
+    ),
+  ).toEqual(expected2);
+});
+test.skip("checking stylish formatter with files with '.yaml' extensions", () => {
+  expect(
+    gendiff(
+      absolutePath('__fixtures__/newfile3.yaml'),
+      absolutePath('__fixtures__/newfile4.yml'),
+      'stylish',
+    ),
+  ).toEqual(expected2);
+});
 
 test('checking plain formatter with files of different extensions', () => {
   expect(
--- buildDiff.js
+++ buildDiff.js
@@ -9,31 +9,30 @@ const getKeys = (file1, file2) => {
 
 const diffData = (f1, f2) => {
   const keys = getKeys(f1, f2);
-  return keys
-    .map((key) => {
-      if (_.has(f1, key) && !_.has(f2, key)) {
-        return { key, value: f1[key], status: 'deleted' };
-      }
-      if (_.has(f2, key) && !_.has(f1, key)) {
-        return { key, value: f2[key], status: 'added' };
-      }
-      if (_.isObject(f1[key]) && _.isObject(f2[key])) {
-        return {
-          key,
-          children: diffData(f1[key], f2[key]),
-          status: 'nested',
-        };
-      }
-      return f1[key] === f2[key]
-        ? { key, value: f1[key], status: 'unchanged' }
-        : {
-          key,
-          oldValue: f1[key],
-          newValue: f2[key],
-          status: 'changed',
-        };
-    })
-    .sort((a, b) => (a.key > b.key ? 1 : -1));
+  const sortedKeys = keys.map((key) => {
+    if (_.has(f1, key) && !_.has(f2, key)) {
+      return { key, value: f1[key], status: 'deleted' };
+    }
+    if (_.has(f2, key) && !_.has(f1, key)) {
+      return { key, value: f2[key], status: 'added' };
+    }
+    if (_.isObject(f1[key]) && _.isObject(f2[key])) {
+      return {
+        key,
+        children: diffData(f1[key], f2[key]),
+        status: 'nested',
+      };
+    }
+    return f1[key] === f2[key]
+      ? { key, value: f1[key], status: 'unchanged' }
+      : {
+        key,
+        oldValue: f1[key],
+        newValue: f2[key],
+        status: 'changed',
+      };
+  });
+  return sortedKeys.sort((a, b) => (a.key > b.key ? 1 : -1));
 };
 
 export default diffData;
